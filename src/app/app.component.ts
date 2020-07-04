efd1803cc8efdaeb58786ca381ee9d2a915556f8
Jacek Olearczyk <rurku@rurku.com>
2020-07-04 23:07:23 +0200

    Add dynamic form array with add and remove buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  DynamicFormModel,
+  DynamicCheckboxModel,
+  DynamicInputModel,
+  DynamicRadioGroupModel,
+  DynamicFormService,
+  DynamicFormArrayModel
+} from "@ng-dynamic-forms/core";
+import { FormGroup, FormArray } from '@angular/forms';
+
+export const MY_FORM_MODEL: DynamicFormModel = [
+
+  new DynamicFormArrayModel({
+    id: "myFormArray",
+    groupFactory: () => {
+      return [
+          new DynamicInputModel({
+              id: "myInput",
+              label: "My Input"
+          })
+      ];
+  }
+  })
+];
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-dynamic-forms-bug';
+export class AppComponent implements OnInit {
+
+  formModel: DynamicFormModel = MY_FORM_MODEL;
+  formGroup: FormGroup;
+  formArrayControl: FormArray;
+  formArrayModel: DynamicFormArrayModel;
+
+  constructor(private formService: DynamicFormService) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+    this.formArrayModel = this.formService.findModelById<DynamicFormArrayModel>("myFormArray", this.formModel);
+    this.formArrayControl = this.formService.findControlByModel<FormArray>(this.formArrayModel, this.formGroup);
+  }
+
+  removeItem(context: DynamicFormArrayModel, index: number) {
+    this.formService.removeFormArrayGroup(index, this.formArrayControl, context);
+    this.formService.detectChanges();
+  }
+
+  addItem() {
+    this.formService.addFormArrayGroup(this.formArrayControl, this.formArrayModel);
+    this.formService.detectChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DynamicFormsBasicUIModule } from '@ng-dynamic-forms/ui-basic';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    BrowserModule,
+    DynamicFormsBasicUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
